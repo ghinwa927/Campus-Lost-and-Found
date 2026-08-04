ae1cdab3b41865560eb004ca78739fdd2438cf64
ghinwa927 <ghinwaabousahyoun9@gmail.com>
2026-08-04 10:34:38 +0300

    Initial setup

---
+++ frontend/app/components/layout/AppFooter.vue
@@ -0,0 +1,6 @@
+<template>
+  <footer class="border-t p-4 text-center flex flex-col items-center justify-center">
+    <p class="text-blue-900">Campus Lost & Found</p>
+    <p>&copy; 2026 Campus Lost & Found</p>
+  </footer>
+</template>
---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Campus Lost & Found</h1>
+    <p>Find your lost items before it's gone.</p>
+  </div>
+</template>
+
+<script></script>
